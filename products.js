ac2a3466246d76632a347f9bf7486c9204fd538c
xueying0807110 <xueying0807110@gmail.com>
2026-07-21 11:16:09 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    id: 1,
+    name: "Halden",
+    image: "./product-images/Halden.png",
+    alt: "Halden wireless over-ear headphones"
+  },
+  {
+    id: 2,
+    name: "Kellan",
+    image: "./product-images/Kellan.png",
+    alt: "Kellan wireless over-ear headphones"
+  },
+  {
+    id: 3,
+    name: "Norwyn",
+    image: "./product-images/Norwyn.png",
+    alt: "Norwyn wireless over-ear headphones"
+  },
+  {
+    id: 4,
+    name: "Marlow",
+    image: "./product-images/Marlow.png",
+    alt: "Marlow wireless over-ear headphones"
+  },
+  {
+    id: 5,
+    name: "Rowen",
+    image: "./product-images/Rowen.png",
+    alt: "Rowen wireless over-ear headphones"
+  },
+  {
+    id: 6,
+    name: "Vanton",
+    image: "./product-images/Vanton.png",
+    alt: "Vanton wireless over-ear headphones"
+  },
+  {
+    id: 7,
+    name: "Arden",
+    image: "./product-images/Arden.png",
+    alt: "Arden wireless over-ear headphones"
+  },
+  {
+    id: 8,
+    name: "Bellmore",
+    image: "./product-images/Bellmore.png",
+    alt: "Bellmore wireless over-ear headphones"
+  },
+  {
+    id: 9,
+    name: "Calder",
+    image: "./product-images/Calder.png",
+    alt: "Calder wireless over-ear headphones"
+  }
+];
+
+window.products = products;
