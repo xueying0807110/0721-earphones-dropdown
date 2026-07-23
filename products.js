2c82386eebb6bb91086369bab10aab5d6ffed746
xueying0807110 <xueying0807110@gmail.com>
2026-07-23 18:19:12 +0800

    Add files via upload

--- products.js
+++ products.js
@@ -2,55 +2,55 @@ const products = [
   {
     id: 1,
     name: "Halden",
-    image: "./product-images/Halden.png",
+    image: "./product-images/Halden.webp",
     alt: "Halden wireless over-ear headphones"
   },
   {
     id: 2,
     name: "Kellan",
-    image: "./product-images/Kellan.png",
+    image: "./product-images/Kellan.webp",
     alt: "Kellan wireless over-ear headphones"
   },
   {
     id: 3,
     name: "Norwyn",
-    image: "./product-images/Norwyn.png",
+    image: "./product-images/Norwyn.webp",
     alt: "Norwyn wireless over-ear headphones"
   },
   {
     id: 4,
     name: "Marlow",
-    image: "./product-images/Marlow.png",
+    image: "./product-images/Marlow.webp",
     alt: "Marlow wireless over-ear headphones"
   },
   {
     id: 5,
     name: "Rowen",
-    image: "./product-images/Rowen.png",
+    image: "./product-images/Rowen.webp",
     alt: "Rowen wireless over-ear headphones"
   },
   {
     id: 6,
     name: "Vanton",
-    image: "./product-images/Vanton.png",
+    image: "./product-images/Vanton.webp",
     alt: "Vanton wireless over-ear headphones"
   },
   {
     id: 7,
     name: "Arden",
-    image: "./product-images/Arden.png",
+    image: "./product-images/Arden.webp",
     alt: "Arden wireless over-ear headphones"
   },
   {
     id: 8,
     name: "Bellmore",
-    image: "./product-images/Bellmore.png",
+    image: "./product-images/Bellmore.webp",
     alt: "Bellmore wireless over-ear headphones"
   },
   {
     id: 9,
     name: "Calder",
-    image: "./product-images/Calder.png",
+    image: "./product-images/Calder.webp",
     alt: "Calder wireless over-ear headphones"
   }
 ];
